6136d6c6338c2e2fb3fdb0752d5c69cbd4bd805a
Harsh71019 <52976443+Harsh71019@users.noreply.github.com>
2020-03-20 12:03:01 +0530

    hey

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const image = require("./controllers/image");
 const db = knex({
 	client: "pg",
 	connection: {
-		host: "postgresql-trapezoidal-23908",
-		user: "postgres",
-		password: "",
-		database: "smartbrain"
+		host: process.env.DATABASE_URL,
+		ssl: true,
+	
 	}
 });
 
